b9b8b4cbb2aeee3a740d5ac5fc03adbc4e2d9aee
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2025-08-24 15:59:29 +0500

    9in

--- src/app/pages/videos/videos.component.ts
+++ src/app/pages/videos/videos.component.ts
@@ -9,6 +9,7 @@ import { title } from 'node:process';
 })
 export class VideosComponent implements OnInit {
   videos = [ 
+    { id: 'O6Danzfjgzk', title: 'LIST CRUD: For/List/While qaytarish, List CRUD bo\'yicha loyichacha ', type: 'F' },
     { id: 'EUkfJ9MYp8s', title: 'Telegram bot: FLashcart, Lug\'at, Mashg\'ulot va User login', type: 'A' },
     { id: 'wqZkuZ0968E', title: 'Loop: Dart tilida For & While looplari va misollari', type: 'F' },
     { id: 'Cvrv5OhyBZY', title: 'UserClaims: Jwt Token orqali UserId ni avtomatik qiymatlash', type: 'A' },
