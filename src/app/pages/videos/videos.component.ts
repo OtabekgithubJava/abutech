533199df91bee18fd8696d682e569af4f08ed554
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2025-08-26 18:31:49 +0500

    789ij9

--- src/app/pages/videos/videos.component.ts
+++ src/app/pages/videos/videos.component.ts
@@ -9,6 +9,7 @@ import { title } from 'node:process';
 })
 export class VideosComponent implements OnInit {
   videos = [ 
+    { id: 'n_R1Il9JFVw', title: 'Funksiya: Dart tilida kurslari ro\'yhati CRUDi bo\'yicha misol', type: 'F' },
     { id: 'WaW2W_SUh6k', title: 'Telegram bot 2: FLashcart, Lug\'at, Mashg\'ulot va User login', type: 'C#' },
     { id: 'O6Danzfjgzk', title: 'LIST CRUD: For/List/While qaytarish, List CRUD bo\'yicha loyichacha ', type: 'F' },
     { id: 'EUkfJ9MYp8s', title: 'Telegram bot: FLashcart, Lug\'at, Mashg\'ulot va User login', type: 'C#' },
