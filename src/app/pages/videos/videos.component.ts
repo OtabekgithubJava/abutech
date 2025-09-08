570f4345757a8fd7e1c62d520e4d6e20534609cc
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2025-09-08 09:00:29 +0500

    idcud8vinv2

--- src/app/pages/videos/videos.component.ts
+++ src/app/pages/videos/videos.component.ts
@@ -9,6 +9,7 @@ import { title } from 'node:process';
 })
 export class VideosComponent implements OnInit {
   videos = [ 
+    { id: 'QN2y-OQpFdI', title: 'Topshiriq javoblari: Polindrome, Teskari for, string concotenation', type: 'F' },
     { id: 'AeH8RxRhcng', title: 'Exam Recap: Dart tili birinchi oy imtihon topshirig\'i 0dan', type: 'F' },
     { id: 'KjKyiW_FZF4', title: 'MicroServices & Deployment: MicroServiceda RUN qilish va serverlar', type: 'A' },
     { id: 'n_R1Il9JFVw', title: 'Funksiya: Dart tilida kurslari ro\'yhati CRUDi bo\'yicha misol', type: 'F' },
