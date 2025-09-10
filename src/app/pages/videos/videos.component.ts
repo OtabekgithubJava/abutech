dedd26b2de98a8c966a7f4041e683a396434a4a8
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2025-09-10 08:51:27 +0500

    8hjbj8c2

--- src/app/pages/videos/videos.component.ts
+++ src/app/pages/videos/videos.component.ts
@@ -9,6 +9,7 @@ import { title } from 'node:process';
 })
 export class VideosComponent implements OnInit {
   videos = [ 
+    { id: 'Tk8dbTOp3DU', title: 'OOP Introduction: Class, Object, Funksiya, Constructor va misollar', type: 'F' },
     { id: 'QN2y-OQpFdI', title: 'Topshiriq javoblari: Polindrome, Teskari for, string concotenation', type: 'F' },
     { id: 'AeH8RxRhcng', title: 'Exam Recap: Dart tili birinchi oy imtihon topshirig\'i 0dan', type: 'F' },
     { id: 'KjKyiW_FZF4', title: 'MicroServices & Deployment: MicroServiceda RUN qilish va serverlar', type: 'A' },
