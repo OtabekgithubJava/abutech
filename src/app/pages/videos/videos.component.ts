2b5b4715d854b00f0a0d564c3577d689e636fd23
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2025-09-14 17:53:54 +0500

    789ojnk2

--- src/app/pages/videos/videos.component.ts
+++ src/app/pages/videos/videos.component.ts
@@ -9,6 +9,7 @@ import { title } from 'node:process';
 })
 export class VideosComponent implements OnInit {
   videos = [ 
+    { id: 'ecKW7QT0WLs', title: 'Scaffold: Android studioda 0dan => appBar, font, icon, colors, & funksiya', type: 'F' },
     { id: 'HZcW97Ae1fk', title: 'Inheritance: OOPda classdan classga voris olish tushunchasi va misol', type: 'F' },
     { id: 'Tk8dbTOp3DU', title: 'OOP Introduction: Class, Object, Funksiya, Constructor va misollar', type: 'F' },
     { id: 'QN2y-OQpFdI', title: 'Topshiriq javoblari: Polindrome, Teskari for, string concotenation', type: 'F' },
