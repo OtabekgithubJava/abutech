c1e2858ec7091fea2975b787f7e6bf03093f1ceb
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2025-12-09 10:04:54 +0500

    t6789ijkn222

--- src/app/pages/videos/videos.component.ts
+++ src/app/pages/videos/videos.component.ts
@@ -9,6 +9,7 @@ import { title } from 'node:process';
 })
 export class VideosComponent implements OnInit {
   videos = [ 
+    { id: 'cPhBF337prQ', title: 'Flutter Form: Flutterda anketaga validator qo\'shish, appBar to\'liq va camera darsi qaytarish', type: 'F' },
     { id: 'MmMaCqjpryc', title: 'Imtihonga tayyorlanish uchun Apple loyihasi | 2-qism: Drawer, BottomNavigation & HomePage', type: 'F' },
     { id: 'q3ESZAGt7ZA', title: 'Imtihonga tayyorlanish uchun Apple loyihasi | 1-qism: Plan qilish va Register page', type: 'F' },
     { id: 'M6dKIs9pj1w', title: 'Camera & Gallareya: Flutterda Drawer qismiga profil rasmni kamera yoki gallareyadan tanlash', type: 'F' },
